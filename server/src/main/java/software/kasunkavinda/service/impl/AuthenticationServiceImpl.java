1c457b94db77721c8991759b7b30a7d9d7549507
KasunKVI <kkhettiarachchi10@gmail.com>
2024-05-03 09:57:01 +0530

    implement the AuthenticationService class

---
+++ server/src/main/java/software/kasunkavinda/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,58 @@
+package software.kasunkavinda.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import software.kasunkavinda.dao.UserRepo;
+import software.kasunkavinda.dto.UserDTO;
+import software.kasunkavinda.entity.UserEntity;
+import software.kasunkavinda.reqAndresp.response.JwtAuthResponse;
+import software.kasunkavinda.reqAndresp.secure.SignIn;
+import software.kasunkavinda.reqAndresp.secure.SignUp;
+import software.kasunkavinda.service.AuthenticationService;
+import software.kasunkavinda.service.JwtService;
+import software.kasunkavinda.util.Mapping;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final UserRepo userRepo;
+    private final JwtService jwtService;
+
+
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final Mapping mapper;
+    @Override
+    public JwtAuthResponse signIn(SignIn signIn) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(signIn.getEmail(),signIn.getPassword()));
+        var userByEmail = userRepo.findByEmail(signIn.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        var generatedToken = jwtService.generateToken(userByEmail);
+        return JwtAuthResponse.builder().token(generatedToken).build() ;
+    }
+
+    @Override
+    public JwtAuthResponse signUp(SignUp signUp) {
+
+        System.out.println(signUp.getId());
+        var buildUser = UserDTO.builder()
+                .id(UUID.randomUUID().toString())
+                .email(signUp.getEmail())
+                .password(passwordEncoder.encode(signUp.getPassword()))
+                .role(signUp.getRole())
+                .build();
+        var savedUser = userRepo.save(mapper.toUserEntity(buildUser));
+        var genToken = jwtService.generateToken(savedUser);
+        return JwtAuthResponse.builder().token(genToken).build();
+    }
+}
