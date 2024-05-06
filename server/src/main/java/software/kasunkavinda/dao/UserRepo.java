1e0433a10a78896e78455fb1577e2c167852308d
KasunKVI <kkhettiarachchi10@gmail.com>
2024-05-06 22:32:30 +0530

    implement the user repo

---
+++ server/src/main/java/software/kasunkavinda/dao/UserRepo.java
@@ -0,0 +1,13 @@
+package software.kasunkavinda.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import software.kasunkavinda.entity.UserEntity;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserEntity, String> {
+    Optional<UserEntity> findByEmail(String email);
+
+}
