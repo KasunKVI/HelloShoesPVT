3571e4485348d020ce54197ae29bcecb40da9c48
KasunKVI <kkhettiarachchi10@gmail.com>
2024-05-16 23:14:30 +0530

    implement the customer controller

--- client/controller/customerController.js
+++ client/controller/customerController.js
@@ -61,34 +61,30 @@ $("#customer_add_btn").click(async function (event) {
     }
 });
 
-$("#customer_search_btn").click(async function (event) {
+async function searchCustomer(customerId) {
 
     // Retrieve the access token from localStorage
     const accessToken = localStorage.getItem('accessToken');
 
-    var nic = $("#customer_nic_search").val();
-
-
     try {
         const response = await $.ajax({
             type: "GET",
-            url: "http://localhost:8080/helloShoesPVT/api/v1/customer/" + nic,
+            url: "http://localhost:8080/helloShoesPVT/api/v1/customer/" + customerId,
             headers: {
                 "Authorization": "Bearer " + accessToken
             },
             contentType: "application/json"
         });
 
-        Swal.fire({
-            icon: "success",
-            title: "Customer Founded!",
-            showConfirmButton: false,
-            timer: 1500
-        });
         // Populate the form fields with the retrieved customer details
+        $("#customer_nic_update").val(response.customer_id);
         $("#customer_name_update").val(response.name);
         $("#customer_email_update").val(response.email);
-        $("#customer_address_update").val(response.building_no+ ", " + response.lane + ", " + response.city + ", " + response.state + ", " + response.postal_code);
+        $("#customer_building_no_update").val(response.building_no);
+        $("#customer_lane_update").val(response.lane);
+        $("#customer_city_update").val(response.city);
+        $("#customer_state_update").val(response.state);
+        $("#customer_postcode_update").val(response.postal_code);
         $("#customer_gender_update").val(response.gender);
         points = response.points;
         level = response.level;
@@ -105,12 +101,9 @@ $("#customer_search_btn").click(async function (event) {
 
         var updateButtonText = document.getElementById("customer_btn_update").textContent;
 
-        if (updateButtonText === "Delete") {
 
-        }else {
             // Display the customer details section
             $("#customer_details_section").show();
-        }
 
 
     } catch (error) {
@@ -124,71 +117,20 @@ $("#customer_search_btn").click(async function (event) {
 
         $("#customer_details_section").hide();
     }
-});
+};
 $("#customer_btn_update").click(async function (event) {
-
-    // Get the text of the Update button
-    var updateButtonText = document.getElementById("customer_btn_update").textContent;
 
     // Retrieve the access token from localStorage
     const accessToken = localStorage.getItem('accessToken');
 
-    var nic = $("#customer_nic_search").val();
-
-    if (updateButtonText === "Delete") {
-
-        Swal.fire({
-            title: "Please Search first for ensure that the customer is in the database",
-            showClass: {
-                popup: `
-      animate__animated
-      animate__fadeInUp
-      animate__faster
-    `
-            },
-            hideClass: {
-                popup: `
-      animate__animated
-      animate__fadeOutDown
-      animate__faster
-    `
-            }
-        });
-
-        try {
-            const response = await $.ajax({
-                type: "DELETE",
-                url: "http://localhost:8080/helloShoesPVT/api/v1/customer/" + nic,
-                headers: {
-                    "Authorization": "Bearer " + accessToken
-                },
-                contentType: "application/json"
-            });
-            Swal.fire({
-                icon: "success",
-                title: "Customer has been deleted",
-                showConfirmButton: false,
-                timer: 1500
-            });
-
-            loadCustomers();
-
-        } catch (error) {
-            console.error("Request failed:", error);
-
-            Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "Customer not found!",
-            });
-        }
-
-    }else if(updateButtonText === "Update"){
-
-
+        var nic = $("#customer_nic_update").val();
         var email = $("#customer_email_update").val();
         var name = $("#customer_name_update").val();
-        var address = $("#customer_address_update").val();
+        var bulding_no = $("#customer_building_no_update").val();
+        var lane = $("#customer_lane_update").val();
+        var city = $("#customer_city_update").val();
+        var state = $("#customer_state_update").val();
+        var postalCode = $("#customer_postcode_update").val();
         var dob = $("#customer_dob_update").val();
         var gender = $("#customer_gender_update").val();
 
@@ -208,7 +150,11 @@ $("#customer_btn_update").click(async function (event) {
                     dob: dob,
                     level: level,
                     points: points,
-                    address: address,
+                    building_no:bulding_no,
+                    lane:lane,
+                    city:city,
+                    state:state,
+                    postal_code:postalCode,
                     email: email
                 }),
                 contentType: "application/json"
@@ -220,29 +166,23 @@ $("#customer_btn_update").click(async function (event) {
                 timer: 1500
             });
             loadCustomers();
-            clearUpdateForm();
+            await searchCustomer(nic);
         } catch (error) {
             console.error("Request failed:", error);
         }
-
-    }
-
-
-
 
 });
 
 // Define the clear update form function
 function clearUpdateForm() {
     // Clear the input values of all form fields
+    $("#customer_nic_update").val("");
     $("#customer_name_update").val("");
     $("#customer_email_update").val("");
     $("#customer_address_update").val("");
     $("#customer_dob_update").val("");
     $("#customer_gender_update").val(""); // Assuming this should be reset to the default value
 
-    // Hide the customer details section
-    $("#customer_details_section").hide();
 }
 
 $("#customer_btn_clear").click(function() {
@@ -270,57 +210,7 @@ $("#customer_btn_clear_add").click(function() {
     clearAddForm();
 });
 
-$("#customer_info_btn").click(function() {
-
-    // Change the modal title to "Customer Info"
-    $("#popupModalLabelCustomerUpdate").text("Customer Info");
-
-    // Hide the Update button
-    $("#customer_btn_update").hide();
-
-    // Hide the Clear button
-    $("#customer_btn_clear_update").hide();
-
-    // Disable all input fields
-    $("#customer_name_update, #customer_email_update, #customer_address_update, #customer_dob_update, #customer_gender_update").prop("disabled", true);
-});
-$("#customer_update_btn").click(function() {
-
-    // Change the modal title to "Update Customer"
-    $("#popupModalLabelCustomerUpdate").text("Update Customer");
-
-    // Show the Update button
-    $("#customer_btn_update").show();
-
-    // Hide the customer details section
-    $("#customer_details_section").hide();
-
-    // Enable all input fields
-    $("#customer_name_update, #customer_email_update, #customer_address_update, #customer_dob_update, #customer_gender_update").prop("disabled", false);
-});
-
-$("#customer_delete_btn").click(function(event) {
-
-    event.preventDefault()
-
-    // Change the modal title to "Update Customer"
-    $("#popupModalLabelCustomerUpdate").text("Delete Customer");
-
-    // Change the label of the Clear button to "Close"
-    $("#customer_btn_update").text("Delete");
-
-    // Hide the customer details section
-    $("#input_section_update").hide();
-    //
-    // // Show the Update button
-    // $("#customer_btn_update").show();
-
-    // // Enable all input fields
-    // $("#customer_name_update, #customer_email_update, #customer_address_update, #customer_dob_update, #customer_gender_update").hide();
-});
-
 $("#customers_link").click(function(event) {
-    console.log("Customer link clicked");
     loadCustomers();
 });
 const loadCustomers = () => {
@@ -342,10 +232,32 @@ const loadCustomers = () => {
 
 
                 response.map((customer, index) => {
-                    let tbl_row = `<tr><td class="customer_id"><p>${customer.customer_id}</p></td><td class="customer_name"><p class="text-xs font-weight-bold mb-0">${customer.name}</p></td><td class="customer_contact_no"><p class="text-center  mb-0">${customer.gender}</p></td><td class="customer_dob"><p class="text-center  mb-0">${customer.joined_date}</p></td><td class="customer_email align-middle text-center text-sm"><span>${customer.dob}</span></td><td class="customer_gender align-middle text-center"><span class="badge badge-sm bg-gradient-success">${customer.level}</span></td><td class="customer_address"><p class="text-center  mb-0">${customer.points}</p></td><td class="customer_address"><p class="text-center  mb-0">${customer.building_no+ ", " + customer.lane + ", " + customer.city + ", " + customer.state + ", " + customer.postal_code}</p></td><<td class="customer_email"><p class="text-center  mb-0">${customer.email}</p></td>/tr>`;
+
+                    // Parse the joined_date to extract year, month, and date
+                    const joinedDate = new Date(customer.joined_date);
+                    const formattedJoinedDate = `${joinedDate.getFullYear()}-${joinedDate.getMonth() + 1}-${joinedDate.getDate()}`;
+
+                    // Parse the dob to extract year, month, and date
+                    const dobDate = new Date(customer.dob);
+                    const formattedDobDate = `${dobDate.getFullYear()}-${dobDate.getMonth() + 1}-${dobDate.getDate()}`;
+
+                    let tbl_row = `<tr><td class="customer_id"><p>${customer.customer_id}</p></td><td class="customer_name"><p class="text-xs font-weight-bold mb-0">${customer.name}</p></td><td class="customer_contact_no"><p class="text-center  mb-0">${customer.gender}</p></td><td class="customer_dob"><p class="text-center  mb-0">${formattedJoinedDate}</p></td><td class="customer_email align-middle text-center text-sm"><span>${formattedDobDate}</span></td><td class="customer_gender align-middle text-center"><span class="badge badge-sm bg-gradient-success">${customer.level}</span></td><td class="customer_address"><p class="text-center  mb-0">${customer.points}</p></td><td class="customer_address"><p class="text-center  mb-0">${customer.building_no+ ", " + customer.lane + ", " + customer.city + ", " + customer.state + ", " + customer.postal_code}</p></td><<td class="customer_email"><p class="text-center  mb-0">${customer.email}</p></td>
+            <td class="align-middle white-space-nowrap text-end pe-0">
+            <div class="btn-reveal-trigger position-static align-middle" >
+            <button class="btn btn-sm dropdown-toggle dropdown-caret-none transition-none btn-reveal fs-10" type="button" data-bs-toggle="dropdown" data-boundary="window" aria-haspopup="true" aria-expanded="false" data-bs-reference="parent"><span class="fas fa-ellipsis-h fs-10"></span></button>
+              <div class="dropdown-menu dropdown-menu-end py-2">
+              <a class="dropdown-item view-action" href="#!" data-bs-toggle="modal" data-bs-target="#updateModelCustomer" data-customer-id="${customer.customer_id}">View</a>
+              <a class="dropdown-item  update-action text-danger" href="#!" data-bs-toggle="modal" data-bs-target="#updateModelCustomer" data-customer-id="${customer.customer_id}">Update</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item delete-action text-warning" href="#!" data-bs-target="#updateModelCustomer" id="customer_delete_btn" data-customer-id="${customer.customer_id}">Remove</a>
+              </div>
+            </div>
+          </td>
+                         </tr>`;
                     $('#customer_table_body').append(tbl_row);
                 });
 
+                attachEventListeners();
 
             },
             error: function (xhr, status, error) {
@@ -356,5 +268,152 @@ const loadCustomers = () => {
 
 };
 
+// Function to attach event listeners
+const attachEventListeners = () => {
+    // Event listener for view action
+    $('.view-action').click(function (e) {
+        e.preventDefault();
+        const customerId = $(this).data('customer-id');
 
+        // Change the modal title to "Customer Info"
+        $("#popupModalLabelCustomerUpdate").text("Customer Info");
+
+        // Hide the Update button
+        $("#customer_btn_update").hide();
+
+        // Hide the Clear button
+        $("#customer_btn_clear_update").hide();
+
+        // Disable all input fields
+        $("#customer_name_update, #customer_email_update, #customer_postcode_update,#customer_state_update,#customer_lane_update,#customer_building_no_update,#customer_city_update, #customer_dob_update, #customer_gender_update, #customer_nic_update").prop("disabled", true);
+
+        searchCustomer(customerId);
+
+
+
+    });
+
+    // Event listener for update action
+    $('.update-action').click(function (e) {
+
+        e.preventDefault();
+
+        const customerId = $(this).data('customer-id');
+
+        console.log(customerId)
+        // Change the modal title to "Update Customer"
+        $("#popupModalLabelCustomerUpdate").text("Update Customer");
+
+        // Show the Update button
+        $("#customer_btn_update").show();
+
+        // Enable all input fields
+        $("#customer_name_update, #customer_email_update, #customer_postcode_update,#customer_state_update,#customer_lane_update,#customer_building_no_update,#customer_city_update, #customer_dob_update, #customer_gender_update").prop("disabled", false);
+
+        searchCustomer(customerId);
+
+    });
+
+    // Event listener for delete action
+    $('.delete-action').click(async function (e) {
+        e.preventDefault();
+        const customerId = $(this).data('customer-id');
+        // Retrieve the access token from localStorage
+        const accessToken = localStorage.getItem('accessToken');
+
+
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true
+        }).then(async (result) => {
+
+            if (result.isConfirmed) {
+
+                try {
+                    const response = await $.ajax({
+                        type: "DELETE",
+                        url: "http://localhost:8080/helloShoesPVT/api/v1/customer/" + customerId,
+                        headers: {
+                            "Authorization": "Bearer " + accessToken
+                        },
+                        contentType: "application/json"
+                    });
+
+                    swalWithBootstrapButtons.fire({
+                        title: "Deleted!",
+                        text: "Customer has been deleted.",
+                        icon: "success"
+                    });
+
+                    loadCustomers();
+
+                } catch (error) {
+                    console.error("Request failed:", error);
+
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Customer not found!",
+                    });
+                }
+
+
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire({
+                    title: "Cancelled",
+                    text: "Customer is safe :)",
+                    icon: "error"
+                });
+            }
+        });
+
+
+    });
+};
+
+// Function to scroll to the specified element
+function scrollToElement(element) {
+    element.scrollIntoView({ behavior: "smooth", block: "center" });
+}
+
+// Function to handle search action
+function handleSearch() {
+    const customerId = $('#search_input').val()
+
+    $('#customer_table_body tr').removeClass('highlighted');
+
+    const row = $(`#customer_table_body tr[data-customer-id="${customerId}"]`);
+
+    console.log(row);
+    // if (row.length > 0) {
+        // Highlight the found row
+        row.addClass('highlighted');
+        scrollToElement(row[0]);
+    // } else {
+    //     alert('Customer ID not found');
+    // }
+}
+
+
+// Event listener for Enter key press on the input field
+$('#search_input').keypress(function (e) {
+    if (e.which === 13) {
+        handleSearch();
+    }
+});
 
