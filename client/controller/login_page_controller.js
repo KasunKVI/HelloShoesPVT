966f04715114891750e26838df0ea177150f59e0
KasunKVI <kkhettiarachchi10@gmail.com>
2024-05-30 13:23:27 +0530

    fix some bugs in login

--- client/controller/login_page_controller.js
+++ client/controller/login_page_controller.js
@@ -1,58 +1,69 @@
 var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 var passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/;
 
+function validLogin() {
+    let isValidLogins = true;
+
+    isValidLogins &= validateField($('#login_email'), emailPattern);
+    isValidLogins &= validateField($('#login_password'), passwordPattern);
+
+    // Show custom alert if the form is invalid
+    if (!isValidLogins) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Please correct the highlighted fields'
+        });
+    }
+
+    return isValidLogins;
+}
+function validateField(field, pattern) {
+    if (!pattern.test(field.val().trim())) {
+        field.removeClass('valid').addClass('invalid');
+        return false;
+    } else {
+        field.removeClass('invalid').addClass('valid');
+        return true;
+    }
+}
+
 $("#login_btn").click(async function(event) {
+
     var userName = $("#login_email").val();
     var password = $("#login_password").val();
+    var hashPass = hashPassword(password);
 
-    let isEmailValid = emailPattern.test(userName);
-
-    if (userName && isEmailValid) {
-        let isPasswordValid = passwordPattern.test(password);
-
-        if (password && isPasswordValid) {
-            var hashPass = hashPassword(password);
-
-            try {
-                const response = await $.ajax({
-                    type: "POST",
-                    url: "http://localhost:8080/helloShoesPVT/api/v1/auth/signIn",
-                    data: JSON.stringify({
-                        email: userName,
-                        password: hashPass
-                    }),
-                    contentType: "application/json"
-                });
-
-                const tokenString = response.token;
-                const [accessToken, refreshToken] = tokenString.split(':').map(token => token.trim());
-
-                localStorage.setItem('accessToken', accessToken);
-                localStorage.setItem('refreshToken', refreshToken);
-
-                $('#login_section').fadeOut('slow', function() {
-                    $('#main_dashboard').fadeIn('slow');
-                    $('#sidenav-main').fadeIn('slow');
-                    $('#navbarBlur').fadeIn('slow');
-                });
-            } catch (error) {
-                console.error("Login failed:", error);
-            }
-        }else {
-            Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "Your Password or Email is invalid!",
+    if(validLogin()) {
+        try {
+            const response = await $.ajax({
+                type: "POST",
+                url: "http://localhost:8081/helloShoesPVT/api/v1/auth/signIn",
+                data: JSON.stringify({
+                    email: userName,
+                    password: hashPass
+                }),
+                contentType: "application/json"
             });
+
+            const tokenString = response.token;
+            const [accessToken, refreshToken] = tokenString.split(':').map(token => token.trim());
+
+            localStorage.setItem('accessToken', accessToken);
+            localStorage.setItem('refreshToken', refreshToken);
+
+            // Store the branch name
+            const branchId = response.branchId;
+            localStorage.setItem('branchId', branchId);
+
+            $('#login_section').fadeOut('slow', function () {
+                $('#main_dashboard').fadeIn('slow');
+                $('#sidenav-main').fadeIn('slow');
+                $('#navbarBlur').fadeIn('slow');
+            });
+        } catch (error) {
+            console.error("Login failed:", error);
         }
-    }else{
-
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Your Password or Email is invalid!",
-        });
-
     }
 
 });
@@ -60,3 +71,11 @@ $("#login_btn").click(async function(event) {
 function hashPassword(password){
     return sha256(password);
 }
+
+$('#login_email').on('input', function() {
+    validateField($(this), emailPattern);
+});
+
+$('#login_password').on('input', function() {
+    validateField($(this), passwordPattern);
+});
