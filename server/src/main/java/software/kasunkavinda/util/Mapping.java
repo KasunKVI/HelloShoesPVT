3553cd58ba4818c1d17252de31b1dc30641c849b
KasunKVI <kkhettiarachchi10@gmail.com>
2024-05-31 10:59:47 +0530

    implement some methods in mapper

--- server/src/main/java/software/kasunkavinda/util/Mapping.java
+++ server/src/main/java/software/kasunkavinda/util/Mapping.java
@@ -3,9 +3,12 @@ package software.kasunkavinda.util;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+import software.kasunkavinda.controller.Employee;
 import software.kasunkavinda.dto.*;
 import software.kasunkavinda.entity.*;
+import software.kasunkavinda.enums.Gender;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -42,13 +45,37 @@ public class Mapping {
     }
 
     public EmployeeEntity toEmployeeEntity(EmployeeDTO employeeDTO) {
-        return mapper.map(employeeDTO, EmployeeEntity.class);
+        EmployeeEntity employee = new EmployeeEntity();
+        employee.setEmployee_id(employeeDTO.getEmployee_id());
+        employee.setName(employeeDTO.getName());
+        employee.setProfile_pic(employeeDTO.getProfile_pic());
+        employee.setGender(Gender.valueOf(employeeDTO.getGender()));
+        employee.setBuilding_no(employeeDTO.getBuilding_no());
+        employee.setLane(employeeDTO.getLane());
+        employee.setCity(employeeDTO.getCity());
+        employee.setState(employeeDTO.getState());
+        employee.setPostal_code(employeeDTO.getPostal_code());
+        employee.setStatus(employeeDTO.getStatus());
+        employee.setEmail(employeeDTO.getEmail());
+        employee.setDesignation(employeeDTO.getDesignation());
+        employee.setContact(employeeDTO.getContact());
+        employee.setEmergency_contact(employeeDTO.getEmergency_contact());
+        employee.setJoined_date(employeeDTO.getJoined_date());
+        employee.setDob(employeeDTO.getDob());
+        employee.setGuardian_name(employeeDTO.getGuardian_name());
+
+        return employee;
     }
     public EmployeeDTO toEmployeeDTO(EmployeeEntity employeeEntity) {
-        return  mapper.map(employeeEntity, EmployeeDTO.class);
+        return new EmployeeDTO(employeeEntity.getEmployee_id(), employeeEntity.getName(), employeeEntity.getProfile_pic(), employeeEntity.getGender().toString(), employeeEntity.getBuilding_no(), employeeEntity.getLane(), employeeEntity.getCity(), employeeEntity.getState(), employeeEntity.getPostal_code(), employeeEntity.getStatus(), employeeEntity.getEmail(), employeeEntity.getDesignation(), employeeEntity.getContact(), employeeEntity.getEmergency_contact(), employeeEntity.getJoined_date(), employeeEntity.getDob(), employeeEntity.getGuardian_name(),employeeEntity.getBranch().getBranch_id());
     }
     public List<EmployeeDTO> toEmployeeDtoList(List<EmployeeEntity> employees) {
-        return mapper.map(employees, List.class);
+        List<EmployeeDTO> employeeDTOS = new ArrayList<>();
+        for (EmployeeEntity employee : employees) {
+            employeeDTOS.add(new EmployeeDTO(employee.getEmployee_id(), employee.getName(), employee.getProfile_pic(), employee.getGender().toString(), employee.getBuilding_no(), employee.getLane(), employee.getCity(), employee.getState(), employee.getPostal_code(), employee.getStatus(), employee.getEmail(), employee.getDesignation(), employee.getContact(), employee.getEmergency_contact(), employee.getJoined_date(), employee.getDob(), employee.getGuardian_name(),employee.getBranch().getBranch_id()));
+
+        }
+        return employeeDTOS;
     }
 
     public ShoeEntity toShoeEntity(InventoryDTO inventoryDTO) {
@@ -59,7 +86,6 @@ public class Mapping {
         shoeEntity.setQty(inventoryDTO.getQty());
         shoeEntity.setBought_price(inventoryDTO.getBought_price());
         shoeEntity.setSell_price(inventoryDTO.getSell_price());
-
         return shoeEntity;
     }
     public InventoryDTO shoeToInventoryDTO(ShoeEntity shoeEntity) {
@@ -106,5 +132,15 @@ public class Mapping {
     }
 
 
+    public List<InventoryDTO> toInventoryDTOList(List<ShoeEntity> shoes, List<AccessoriesEntity> accessories) {
 
+        List<InventoryDTO> inventoryDTOS = new ArrayList<>();
+        for (ShoeEntity shoe : shoes) {
+            inventoryDTOS.add(new InventoryDTO(shoe.getShoe_id(), shoe.getDescription(), shoe.getPicture(), shoe.getQty(), shoe.getBought_price(), shoe.getSell_price(), "Shoe",shoe.getSupplier().getSupplier_id()));
+        }
+        for (AccessoriesEntity accessory : accessories) {
+            inventoryDTOS.add(new InventoryDTO(accessory.getAccessories_id(), accessory.getDescription(), accessory.getPicture(), accessory.getQty(), accessory.getBought_price(), accessory.getSell_price(), "Accessory",accessory.getSupplier().getSupplier_id() ));
+        }
+        return inventoryDTOS;
+    }
 }
